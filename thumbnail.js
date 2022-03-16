03860cec3b76b8dbb33d7761c3cb0699ecc118ba
caocyc <101646438+cao-cyc@users.noreply.github.com>
2022-03-16 15:28:21 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,22 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(event) 
-{
+document.addEventListener("DOMContentLoaded", function(event) {
     // 你的所有代码都应该写在这里
     let targetImage = document.querySelector("#smart-image");
-   
-    if (targetImage.classList.contains("small")) {
-    // ...
-      targetImage.addEventListener("click", function() {
-      alert("点击了图片");
-      targetImage.classList.remove("small");
-                                                 });
-} 
-   else 
-{
-    // ...
-     targetImage.addEventListener("click", function() {
-     alert("点击了图片");
-     targetImage.classList.add("small");
-                                                      });
-}
-});
+    targetImage.addEventListener("click", function() {
+        alert("点击了图片");
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+        } else {
+            targetImage.classList.add("small");
+        }
+    });
+    
 });
